e9bdff4f830fc07e07df1b661e75791c9e2cfbb4
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2024-05-19 16:05:28 +0300

    made a basic form that can be improved in the future and is not functional right now.

---
+++ submissionspage/processsubmit.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    let x = document.forms["nuccform"]["domain"].value;
+    let y = document.forms["nuccform"]["DNS"].value;
+    let z = document.forms["nuccform"]["contact"].value;
+    if (x == "" || y == "" || z =="") {
+      alert("We need you to enter your domain, dns adress and your contact before submitting");
+      return false;
+    }
+  }
