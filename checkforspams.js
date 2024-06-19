3c9638953cd746ef40f8cd4743a36b572a3d08c3
CinarBayramic <93824489+CinarBayramic@users.noreply.github.com>
2024-06-19 10:39:56 +0300

    basic domain data type

--- checkforspams.js
+++ checkforspams.js
@@ -1,5 +1,13 @@
 const domainstoprocess = require('./submitdomains.json'); //get domains that are not processed yet.
 
+export class imbadatnaming {
+Constructor(owner,dns,target) {
+this.owner = owner;
+this.dns = dns;
+this.target = target;
+}
+}
+
 export function ownercheck() {
 
 }
